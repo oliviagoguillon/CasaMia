ece0c40ce4257535b481fe2a25a814de3d885a92
oliviagoguillon <65453910+oliviagoguillon@users.noreply.github.com>
2020-06-02 16:44:01 +0200

    Add files via upload

---
+++ formreservation.php
@@ -0,0 +1,32 @@
+<?php
+if(isset($_GET['date'])){
+    $date = $_GET['date'];
+}
+?>
+<html>
+<head>
+<meta charset="utf-8" />
+<link rel="stylesheet" type="text/css" href="../CSS/reservation.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center">Préréserver pour la date: <?php echo date('m/d/Y', strtotime($date)); ?></h1><hr>
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <form action="" method="post" autocomplete="off">
+                    <div class="form-group">
+                        <label for="">Nom et prénom </label>
+                        <input type="text" class="form-control" name="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Email</label>
+                        <input type="email" class="form-control" name="email">
+                    </div>
+                    <button class="btn btn-primary" type="submit" name="submit">Valider</button>
+                </form>
+            </div>
+        </div>
+    </div>
